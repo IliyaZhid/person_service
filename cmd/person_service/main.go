7953b41dfeeaaa7aeb0d868076a06239e88128b6
Iliya Zhidkikh <iliyazhid@mail.ru>
2025-04-28 22:40:23 +0700

    Добавил работу с логами

--- cmd/person_service/main.go
+++ cmd/person_service/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log/slog"
+	"person_service/internal/config"
+	"person_service/internal/logger"
+)
+
 func main() {
+	cfg := config.MustLoad()
+	log := logger.Setup(cfg.Environment)
+
+	log.Info("Starting person_service", slog.String("environment", cfg.Environment))
+
+	if cfg.Environment == config.EnvLocal || cfg.Environment == config.EnvDev {
+		log.Debug("Debug messages are enabled")
+	}
 
 }
